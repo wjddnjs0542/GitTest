8dd99556d3aa3bf0a18f87646a6728e42dc8d416
Jungwon <wjddnjs1115@nomadstar.com>
2016-11-01 15:57:24 +0900

    Second 로그제거

--- app/src/main/java/com/example/admin/qweqwe/SecondActivity.java
+++ app/src/main/java/com/example/admin/qweqwe/SecondActivity.java
@@ -21,7 +21,7 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_activity);
-        Log.e("TAG","TEMP");
+
         bt = (Button) findViewById(R.id.next);
         bt.setOnClickListener(new View.OnClickListener() {
             @Override
