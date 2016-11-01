274ae319d5a90ec6d18938d4244ee599993ac9bd
Jungwon <wjddnjs1115@nomadstar.com>
2016-11-01 15:23:07 +0900

    로그추가

--- app/src/main/java/com/example/admin/qweqwe/MainActivity.java
+++ app/src/main/java/com/example/admin/qweqwe/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -22,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
+        Log.e("TAG","MSG");
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
