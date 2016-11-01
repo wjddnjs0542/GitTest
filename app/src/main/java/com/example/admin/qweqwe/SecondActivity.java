33556545c8b8e8d305fe95906d215338c74c192e
Jungwon <wjddnjs1115@nomadstar.com>
2016-11-01 15:45:59 +0900

    Second 로그 추가

--- app/src/main/java/com/example/admin/qweqwe/SecondActivity.java
+++ app/src/main/java/com/example/admin/qweqwe/SecondActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -20,7 +21,7 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_activity);
-
+        Log.e("TAG","TEMP");
         bt = (Button) findViewById(R.id.next);
         bt.setOnClickListener(new View.OnClickListener() {
             @Override
